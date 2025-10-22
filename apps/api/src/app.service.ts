aca91ef284c338b3a7cd25aa99c5d8ac382e5797
yu-zhoukai <yuzhoukai7@gmail.com>
2025-10-22 19:51:56 +0800

    new: add prisma

--- apps/api/src/app.service.ts
+++ apps/api/src/app.service.ts
@@ -1,8 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from './prisma.service';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private prisma: PrismaService) { } // 注入PrismaService
+
+  async getHello(): Promise<string> {
+    // 查询数据库里所有的笔记
+    const notes = await this.prisma.note.findMany();
+
+    // 如果没有笔记，就创建一条
+    if (notes.length === 0) {
+      await this.prisma.note.create({
+        data: {
+          title: 'My First Note',
+          content: 'This is a test note from the database.',
+        },
+      });
+
+      return "No notes found, so created one."
+    }
+
+    return `Data from DB: ${notes[0].title}`;
   }
 }
